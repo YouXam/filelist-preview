3a49a8466ec2fcb5254ca0fc14720d6519117a8b
YouXam <youxam@outlook.com>
2025-01-13 02:33:49 +0800

    use space instead of tab
    Signed-off-by: YouXam <youxam@outlook.com>

--- src/svg.ts
+++ src/svg.ts
@@ -8,52 +8,52 @@ let fontsCache: Record<string, ArrayBuffer> | null = null
 async function loadFonts(c: Context) {
     if (fontsCache) return fontsCache
     const fonts: Record<string, ArrayBuffer> = {}
-	const [
-		extraLight,
-		bold
-	] = await Promise.all([
-		c.env.ASSETS.fetch("https://static.local/fonts/NotoSansSC-ExtraLight.ttf"),
-		c.env.ASSETS.fetch("https://static.local/fonts/NotoSansSC-Bold.ttf"),
-	])
-	const [
-		extraLightFont,
-		boldFont
-	] = await Promise.all([
-		extraLight.arrayBuffer(),
-		bold.arrayBuffer(),
-	])
-	fonts.extraLight = extraLightFont
-	fonts.bold = boldFont
+    const [
+        extraLight,
+        bold
+    ] = await Promise.all([
+        c.env.ASSETS.fetch("https://static.local/fonts/NotoSansSC-ExtraLight.ttf"),
+        c.env.ASSETS.fetch("https://static.local/fonts/NotoSansSC-Bold.ttf"),
+    ])
+    const [
+        extraLightFont,
+        boldFont
+    ] = await Promise.all([
+        extraLight.arrayBuffer(),
+        bold.arrayBuffer(),
+    ])
+    fonts.extraLight = extraLightFont
+    fonts.bold = boldFont
     fontsCache = fonts
     return fonts
 }
 
 
 export async function svg<T extends Context<any, any, ImageBodyType>>(c: T) {
-	const fonts = await loadFonts(c)
+    const fonts = await loadFonts(c)
     const data = c.req.valid('json')
     const fontSize = data.fontSize ?? 22
     const height = data.height ?? 800
     const width = data.width ?? 600
-	return await satori(
-		await template(c, data.files, fontSize, data.title, height),
-		{
-			width,
-			height,
-			fonts: [
-				{
-					name: 'Noto',
-					data: fonts.extraLight,
-					weight: 100,
-					style: 'normal',
-				},
-				{
-					name: 'Noto',
-					weight: 400,
-					data: fonts.bold,
-					style: 'normal'
-				}
-			],
-		},
-	)
+    return await satori(
+        await template(c, data.files, fontSize, data.title, height),
+        {
+            width,
+            height,
+            fonts: [
+                {
+                    name: 'Noto',
+                    data: fonts.extraLight,
+                    weight: 100,
+                    style: 'normal',
+                },
+                {
+                    name: 'Noto',
+                    weight: 400,
+                    data: fonts.bold,
+                    style: 'normal'
+                }
+            ],
+        },
+    )
 }
--- src/template.tsx
+++ src/template.tsx
@@ -6,72 +6,72 @@ import { expand, Node } from './expand';
 const icons = new Map<string, string>();
 const manifest = generateManifest();
 const extraExtensions: Record<string, string> = {
-	ts: 'typescript',
-	js: 'javascript',
-	yaml: 'yaml',
-	yml: 'yaml',
+    ts: 'typescript',
+    js: 'javascript',
+    yaml: 'yaml',
+    yml: 'yaml',
 };
 for (const key in extraExtensions) {
-	manifest!.fileExtensions![key] = extraExtensions?.[key]!;
+    manifest!.fileExtensions![key] = extraExtensions?.[key]!;
 }
 
 function guessIcon(filename: string, type: string = 'file') {
-	if (type === 'file') {
-		if (manifest?.fileNames?.[filename]) {
-			return manifest.fileNames[filename];
-		}
-		const exts = filename.split('.')
+    if (type === 'file') {
+        if (manifest?.fileNames?.[filename]) {
+            return manifest.fileNames[filename];
+        }
+        const exts = filename.split('.')
         while (exts.length > 1) {
             exts.shift()
             if (manifest?.fileExtensions?.[exts.join('.')]) {
                 return manifest.fileExtensions[exts.join('.')];
             }
         }
-		return manifest.file!;
-	} else if (type === 'folder') {
-		if (manifest?.folderNames?.[filename]) {
-			return manifest.folderNames[filename];
-		}
-		return manifest.folder!;
-	} else if (type === 'folderExpanded') {
-		if (manifest?.folderNamesExpanded?.[filename]) {
-			return manifest.folderNamesExpanded[filename];
-		}
-		return manifest.folderExpanded!;
-	}
-	return 'file';
+        return manifest.file!;
+    } else if (type === 'folder') {
+        if (manifest?.folderNames?.[filename]) {
+            return manifest.folderNames[filename];
+        }
+        return manifest.folder!;
+    } else if (type === 'folderExpanded') {
+        if (manifest?.folderNamesExpanded?.[filename]) {
+            return manifest.folderNamesExpanded[filename];
+        }
+        return manifest.folderExpanded!;
+    }
+    return 'file';
 }
 
 async function svg(context: Context, filename: string, type: string, fontSize: number) {
-	const key = guessIcon(filename, type);
-	if (icons.has(key)) {
-		return icons.get(key);
-	}
-	const res = await context.env.ASSETS.fetch(`https://static.local/icons/${key}.json`);
-	const body = JSON.parse(await res.text());
-	body.props.style = {
-		width: `${fontSize}px`,
-		height: `${fontSize}px`,
-	};
-	icons.set(key, body);
-	return body;
+    const key = guessIcon(filename, type);
+    if (icons.has(key)) {
+        return icons.get(key);
+    }
+    const res = await context.env.ASSETS.fetch(`https://static.local/icons/${key}.json`);
+    const body = JSON.parse(await res.text());
+    body.props.style = {
+        width: `${fontSize}px`,
+        height: `${fontSize}px`,
+    };
+    icons.set(key, body);
+    return body;
 }
 async function line(context: Context, fontSize: number, { filename, type, indent }: { filename: string; type: string; indent: number }) {
-	return (
-		<div
-			style={{
-				display: 'flex',
-				paddingLeft: `${(indent - 1) * (fontSize * 1.5)}px`,
-				paddingRight: '8px',
-				overflow: 'hidden',
-				whiteSpace: 'nowrap',
+    return (
+        <div
+            style={{
+                display: 'flex',
+                paddingLeft: `${(indent - 1) * (fontSize * 1.5)}px`,
+                paddingRight: '8px',
+                overflow: 'hidden',
+                whiteSpace: 'nowrap',
                 alignItems: 'center',
-			}}
-		>
-			<span style={{ marginRight: '8px' }}>{await svg(context, filename, type, fontSize)}</span>
-			{filename + ((type === 'folderExpanded' || type === 'folder') && !filename.endsWith('/') ? '/' : '')}
-		</div>
-	);
+            }}
+        >
+            <span style={{ marginRight: '8px' }}>{await svg(context, filename, type, fontSize)}</span>
+            {filename + ((type === 'folderExpanded' || type === 'folder') && !filename.endsWith('/') ? '/' : '')}
+        </div>
+    );
 }
 function omit(fontSize: number, { indent }: { indent: number }) {
     return <div style={{
@@ -99,19 +99,19 @@ const lineRatio = 1.44928;
 
 export const template = async (context: Context, files: Node[], fontSize: number, title: string | undefined, height: number) => {
     const count = Math.floor((height - (title ? fontSize * h2Ratio : 40)) / (fontSize * lineRatio));
-	return (
-		<div
-			style={{
-				display: 'flex',
-				flexDirection: 'column',
-				width: '100%',
-				height: '100%',
+    return (
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                width: '100%',
+                height: '100%',
                 backgroundColor: '#181818',
                 color: '#fff',
                 fontSize: `${fontSize}px`,
-			}}
-		>
-			{title ? <h2 style={{
+            }}
+        >
+            {title ? <h2 style={{
                 textAlign: 'center',
                 width: '100%',
                 justifyContent: 'center',
@@ -119,17 +119,17 @@ export const template = async (context: Context, files: Node[], fontSize: number
             }}>
                 {title}
             </h2> : null}
-			<div
-				style={{
-					display: 'flex',
-					flexDirection: 'column',
-					padding: '20px',
-					paddingTop: title ? '0' : '20px',
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    padding: '20px',
+                    paddingTop: title ? '0' : '20px',
                     overflow: 'hidden',
-				}}
-			>
+                }}
+            >
                 {await render(context, fontSize, count, files)}
-			</div>
-		</div>
-	);
+            </div>
+        </div>
+    );
 };
